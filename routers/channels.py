cbe3fe4be79670b16e9b8c7c6a4e8cbe823aa935
Cristobal Sfeir <cnsfeir@uc.cl>
2023-09-04 20:47:03 -0300

    ✨ Add channels router

---
+++ routers/channels.py
@@ -0,0 +1,34 @@
+from http import HTTPStatus
+from logging import getLogger
+from typing import Annotated, cast
+
+from cumplo_common.database.firestore import firestore_client
+from cumplo_common.models.channel import ChannelConfiguration, ChannelType
+from cumplo_common.models.user import User
+from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
+from fastapi.requests import Request
+
+from business.channels import ChannelConfigurationFactory
+
+logger = getLogger(__name__)
+
+router = APIRouter(prefix="/channels")
+
+
+@router.post("/{channel_type}", status_code=HTTPStatus.CREATED)
+async def get_configurations(
+    request: Request, channel_type: ChannelType, payload: dict
+) -> Annotated[dict, ChannelConfiguration]:
+    """Posts a new channel configuration."""
+    user = cast(User, request.state.user)
+    if not (configuration := ChannelConfigurationFactory.create_channel_configuration(channel_type, payload)):
+        raise HTTPException(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    firestore_client.put_channel(user.id, configuration)
+    return configuration.model_dump(exclude_none=True)
+
+
+# TODO: Add a is_active field to the channel configuration, so we can deactivate it (soft delete)
+# TODO: Make the PATCH endpopint for the configuration
+# TODO: Make the DELETE endpoint for the configuration
