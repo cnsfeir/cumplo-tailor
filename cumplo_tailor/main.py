9171277a138b33371f4f5fcf57cf1ce3eb7fb565
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-31 20:20:06 -0300

    🔥 Remove CORS middleware

---
+++ cumplo_tailor/main.py
@@ -0,0 +1,26 @@
+from logging import CRITICAL, DEBUG, basicConfig, getLogger
+
+import google.cloud.logging
+from cumplo_common.dependencies.authentication import authenticate
+from cumplo_common.middlewares import PubSubMiddleware
+from fastapi import Depends, FastAPI
+
+from cumplo_tailor.routers import configurations
+from cumplo_tailor.utils.constants import IS_TESTING, LOG_FORMAT
+
+basicConfig(level=DEBUG, format=LOG_FORMAT)
+logger = getLogger(__name__)
+
+# NOTE: Mute noisy third-party loggers
+for module in ("google", "urllib3", "werkzeug"):
+    getLogger(module).setLevel(CRITICAL)
+
+if not IS_TESTING:
+    client = google.cloud.logging.Client()
+    client.setup_logging(log_level=DEBUG)
+
+
+app = FastAPI()
+app.add_middleware(PubSubMiddleware)
+
+app.include_router(configurations.router, dependencies=[Depends(authenticate)])
