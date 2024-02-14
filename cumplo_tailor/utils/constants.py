fe6b0d926cad01777028566a4ad31ed1fd2dcdad
Cristobal Sfeir <cnsfeir@uc.cl>
2024-02-14 20:20:41 -0300

    ♻️ Reorder constants

--- cumplo_tailor/utils/constants.py
+++ cumplo_tailor/utils/constants.py
@@ -4,12 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CONFIGURATIONS_COLLECTION = os.getenv("CONFIGURATIONS_COLLECTION", "configurations")
-IS_TESTING = bool(os.getenv("IS_TESTING"))
 
+# Basics
 LOCATION = os.getenv("LOCATION", "us-central1")
-LOG_FORMAT = "\n [%(levelname)s] %(message)s"
-MAX_CONFIGURATIONS = int(os.getenv("MAX_CONFIGURATIONS", "3"))
-NOTIFICATIONS_COLLECTION = os.getenv("NOTIFICATIONS_COLLECTION", "notifications")
 PROJECT_ID = os.getenv("PROJECT_ID")
+IS_TESTING = bool(os.getenv("IS_TESTING"))
+LOG_FORMAT = "\n%(levelname)s: %(message)s"
+
+# Defaults
+MAX_CONFIGURATIONS = int(os.getenv("MAX_CONFIGURATIONS", "3"))
+
+# Firestore Collections
+CONFIGURATIONS_COLLECTION = os.getenv("CONFIGURATIONS_COLLECTION", "configurations")
+NOTIFICATIONS_COLLECTION = os.getenv("NOTIFICATIONS_COLLECTION", "notifications")
 USERS_COLLECTION = os.getenv("USERS_COLLECTION", "users")
