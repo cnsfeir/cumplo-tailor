c1a1c49ca909845ca9ddd704c3616198a58a2b10
Cristobal Sfeir <cnsfeir@uc.cl>
2024-03-23 20:07:20 -0300

    👽️ Update channel configuration field names

--- cumplo_tailor/business/channels.py
+++ cumplo_tailor/business/channels.py
@@ -32,11 +32,11 @@ def validate(user: User, channel: ChannelConfiguration) -> None:
             channel = cast(IFTTTConfiguration, channel)
             ifttt_channels = (channel for channel in user.channels.values() if channel.type_ == ChannelType.IFTTT)
             if existing_channel := next(ifttt_channels, None):
-                if cast(IFTTTConfiguration, existing_channel).metadata.key != channel.metadata.key:
+                if cast(IFTTTConfiguration, existing_channel).key != channel.key:
                     raise HTTPException(HTTPStatus.CONFLICT, detail="Only one IFTTT key is allowed per user")
 
         case ChannelType.WEBHOOK:
             channel = cast(WebhookConfiguration, channel)
             for webhook_channel in filter(lambda channel: channel.type_ == ChannelType.WEBHOOK, user.channels.values()):
-                if cast(WebhookConfiguration, webhook_channel).metadata.url == channel.metadata.url:
+                if cast(WebhookConfiguration, webhook_channel).url == channel.url:
                     raise HTTPException(HTTPStatus.CONFLICT, detail="This webhook URL already exists")
