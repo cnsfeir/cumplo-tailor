440f6f7d69816374f3830a64c6384f40ff37e37d
Cristobal Sfeir <cnsfeir@uc.cl>
2024-03-23 20:08:09 -0300

    ✨ Add util for merging two dicts recursively

---
+++ cumplo_tailor/utils/dictionary.py
@@ -0,0 +1,11 @@
+def update_dictionary(original: dict, new: dict) -> dict:
+    """
+    Recursively merges the new dictionary into the original dictionary overwriting existing values.
+    """
+    for key, value in new.items():
+        if key in original and isinstance(original[key], dict) and isinstance(value, dict):
+            update_dictionary(original[key], value)
+            continue
+
+        original[key] = value
+    return original
