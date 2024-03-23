c665d60d67b6d889c86f6d5c2bdd3f16cf89c441
Cristobal Sfeir <cnsfeir@uc.cl>
2024-03-23 10:50:47 -0300

    ✨ Identify Channels with ULID instead of by type

---
+++ cumplo_tailor/business/channels.py
@@ -0,0 +1,40 @@
+from http import HTTPStatus
+from typing import cast
+
+from cumplo_common.models.channel import (
+    ChannelType,
+    IFTTTConfiguration,
+    WebhookConfiguration,
+    ChannelConfiguration,
+)
+from cumplo_common.models.user import User
+from fastapi.exceptions import HTTPException
+
+
+def validate(user: User, channel: ChannelConfiguration) -> None:
+    """
+    Validates that the user can add the channel based on the user's existing channels.
+
+    Args:
+        channel (ChannelConfiguration): The channel configuration to validate.
+
+    Raises:
+        HTTPException: When a the user already has a WhatsApp channel.
+        HTTPException: When the user already has an IFTTT channel with the a different key.
+        HTTPException: When the user already has a Webhook channel with the same URL.
+    """
+    match channel.type_:
+        case ChannelType.WHATSAPP:
+            if any(channel.type_ == ChannelType.WHATSAPP for channel in user.channels.values()):
+                raise HTTPException(HTTPStatus.CONFLICT, detail="Only one WhatsApp channel is allowed")
+
+        case ChannelType.IFTTT:
+            ifttt_channels = (channel for channel in user.channels.values() if channel.type_ == ChannelType.IFTTT)
+            if existing_channel := next(ifttt_channels, None):
+                if cast(IFTTTConfiguration, existing_channel).key != cast(IFTTTConfiguration, channel).key:
+                    raise HTTPException(HTTPStatus.CONFLICT, detail="Only one IFTTT key is allowed per user")
+
+        case ChannelType.WEBHOOK:
+            for channel in filter(lambda channel: channel.type_ == ChannelType.WEBHOOK, user.channels.values()):
+                if cast(WebhookConfiguration, channel).url == cast(WebhookConfiguration, channel).url:
+                    raise HTTPException(HTTPStatus.CONFLICT, detail="This webhook URL already exists")
